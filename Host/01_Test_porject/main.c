fdf693384acc4cea5ab0e7df4ea88b1cab37af28
Pragneshpatel2023 <patelpragnesh036@gmail.com>
2024-10-31 21:09:25 +0530

    First test project.

---
+++ Host/01_Test_porject/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 31, 2024
+ *      Author: Pragnesh Patel
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("!!!Hello World!!!\n");
+	printf("!!!Hello \t World!!!\n");
+
+	printf("David says , \" Programming is fun !\" \n");
+	printf("**Conditions apply , \"Offers valid until tomorrow\" \n");
+	printf("C:\\My computer\\My folder \n");
+	printf("D:/My documents/My file \n");
+	printf("\\ \\ \\ \\ Today is holiday \\ \\ \\ \\ \n");
+	printf("This is a triple quoted string \"\"\" This month has 30 days \"\"\" \n");
+	return 0;
+}
+
